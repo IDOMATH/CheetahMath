11aa7264e41e3e77b90183f75713540cda548cef
IDOMATH <btdothemath@gmail.com>
2024-08-03 22:13:44 -0500

    Adding expectation to test

--- formulas/haversine_test.go
+++ formulas/haversine_test.go
@@ -1,12 +1,14 @@
 package formulas
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestHaversine(t *testing.T) {
-	// expected := true
+	expected := 0.8750338183720852
 	got := Haversine(51.5007, 0.1246, 40.6892, 74.0445)
-	fmt.Println(got)
+
+	if got != expected {
+		t.Errorf("Expected %v, got: %v", expected, got)
+	}
 }
