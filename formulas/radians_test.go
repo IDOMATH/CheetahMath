7661a9a2afc8c272c457a738dc1f4e8aa89fb24e
IDOMATH <btdothemath@gmail.com>
2024-08-10 05:37:23 -0500

    Adding radians test

---
+++ formulas/radians_test.go
@@ -0,0 +1,15 @@
+package formulas
+
+import (
+	"math"
+	"testing"
+)
+
+func TestRadians(t *testing.T) {
+	expected := math.Pi / 2.0
+	got := ToRadians(90.0)
+
+	if got != expected {
+		t.Errorf("Expected %v, got: %v", expected, got)
+	}
+}
