1002414e0dfebe27a7c6d05927eed557233f9509
IDOMATH <btdothemath@gmail.com>
2024-10-25 18:49:36 -0500

    Fixing tests

--- formulas/int_pow_test.go
+++ formulas/int_pow_test.go
@@ -4,20 +4,20 @@ import "testing"
 
 func TestIntPow(t *testing.T) {
 	expected := 1
-	got, _ := IntPow(2, 0)
+	got := IntPow(2, 0)
 
 	if got != expected {
 		t.Errorf("Expected %v, got: %v", expected, got)
 	}
 
 	expected = 2
-	got, _ = IntPow(2, 1)
+	got = IntPow(2, 1)
 	if got != expected {
 		t.Errorf("Expected %v, got: %v", expected, got)
 	}
 
 	expected = 8
-	got, _ = IntPow(2, 3)
+	got = IntPow(2, 3)
 	if got != expected {
 		t.Errorf("Expected %v, got: %v", expected, got)
 	}
