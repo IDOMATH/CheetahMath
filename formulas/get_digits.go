f5328303d328abafe2c2125297e7aa0dbda313eb
IDOMATH <btdothemath@gmail.com>
2025-06-03 20:55:06 -0500

    Making get_digits better

--- formulas/get_digits.go
+++ formulas/get_digits.go
@@ -1,13 +1,10 @@
 package formulas
 
+import "strconv"
+
 func GetDigits(num int) int {
 	if num < 0 {
 		num *= -1
 	}
-	digits := 0
-	for ok := true; ok; ok = (num > 0) {
-		num /= 10
-		digits++
-	}
-	return digits
+	return len(strconv.Itoa(num))
 }
